ef40fa58a3d6a762a48dd86dddcc193d37597a31
Kyle Napod <knnapod@puc.edu>
2015-04-30 15:10:58 -0700

    2nd Commit
    New Commit

--- ChatServer.rb
+++ ChatServer.rb
@@ -2,7 +2,7 @@ require "socket"
 
 class Server
   def initialize(port, ip)
-    @server = nil
+    @server = TCPServer.open(ip, port)
     @connections  = 
     {
       clients:{ client_name: {attributes}, ...} 
@@ -10,4 +10,26 @@ class Server
     @rooms = {}
     @clients = {}
   end
+  
+  def run
+    loop 
+    {
+     #For each user connected by server, creates a new thread.
+     Thread.start(@server.accept) do | client |
+       name = client.gets.chomp.to_sym
+       @connection[:clients].each do |other_name, other_client|
+         if nick_name == other_name || client == other_client
+           client.puts "This username already exsists."
+           Thread.kill self
+         end
+       end
+       puts "#{nick_name} #{client}"
+       @connection[:clients][nick_name] = client
+       client.puts "You connected! Chat your heart out!"
+     end 
+    }
+  end
+  
 end
+server = Server.new("localhost", 3000)
+server.run
